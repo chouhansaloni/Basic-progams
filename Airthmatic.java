614e57fc4552797f72979b05d9067b10eadade1a
SALONI CHOUHAN <169046581+chouhansaloni@users.noreply.github.com>
2024-08-07 13:48:09 +0530

    Add files via upload

---
+++ Airthmatic.java
@@ -0,0 +1,11 @@
+public class Airthmatic {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
