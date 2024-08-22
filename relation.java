25576d62e15570d6af87ac89c701b92412dfbd48
SALONI CHOUHAN <169046581+chouhansaloni@users.noreply.github.com>
2024-08-22 18:55:05 +0530

    Add files via upload

---
+++ relation.java
@@ -0,0 +1,11 @@
+class Relation{
+    public static void main(String[] args)
+    {
+       int var1 = 5, var2 = 10, var3 = 5;
+        System.out.println("Var1 = " + var1);
+        System.out.println("Var2 = " + var2);
+        System.out.println("Var3 = " + var3);
+        System.out.println("var1 == var2: "+ (var1 == var2));
+        System.out.println("var1 == var3: "+ (var1 == var3));
+    }
+}
