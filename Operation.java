41a22a8dac1a856c2a11dcfa1729f33ad95e7368
SALONI CHOUHAN <169046581+chouhansaloni@users.noreply.github.com>
2024-08-23 20:06:22 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Operation{
+    public static void main(String[] args)
+    {
+
+        int n1 = 2;
+        System.out.println("Number = " + n1);
+         n1 = - n1;
+        System.out.println("Result = " + n1);
+    }
+}
